77e3e1a3305b51221d34bd48d1a81ceb7e50912a
Praveen manchi <praveenmanchi@proton.me>
2024-01-06 01:55:15 +0530

    Seo updated

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -13,7 +13,7 @@ type Presentation = {
 
 const presentation: Presentation = {
   mail: "sahithmanchi08@gmail.com",
-  title: "Hi, I’m Sahith Manchi 👋",
+  title: "Hi, I’m Sahith M 👋",
   // profile: "/profile.webp",
   description:
     "Namaste 🙏🏻, i'm a *Java Full Stack Developer* with over * 5+ years* of Java, Springboot, Spring MVC, Microservices, Angular, React, Azure, AWS with various Database experiences like Oracle, PostgresSql, MongoDB Etc., I can Create, Build and Develop Full Stack Applications.",
